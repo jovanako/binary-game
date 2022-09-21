6e3da4e4c1c1214e0ac4a4ebf58c398484132efe
Jovana Kovacevic <jovana1005@gmail.com>
2022-09-21 12:26:10 +0200

    added style to binary controls

--- src/Components/BinaryControls/BinaryControls.js
+++ src/Components/BinaryControls/BinaryControls.js
@@ -1,16 +1,15 @@
 import BinaryControlItem from "../BinaryControlItem/BinaryControlItem";
+import "./BinaryControls.css";
 
 export default function BinaryControls() {
   const buttonNumbers = [128, 64, 32, 16, 8, 4, 2, 1];
   const numberOfItems = buttonNumbers.length;
 
   return (
-    <div id="binary-control">
-      <div id="binary-control-items">
-        {[...Array(numberOfItems)].map((_, i) => (
-          <BinaryControlItem key={i} buttonNumber={buttonNumbers[i]} />
-        ))}
-      </div>
+    <div id="binary-controls">
+      {[...Array(numberOfItems)].map((_, i) => (
+        <BinaryControlItem key={i} buttonNumber={buttonNumbers[i]} />
+      ))}
     </div>
   );
 }
